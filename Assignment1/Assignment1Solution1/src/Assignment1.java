ece8723df20a4449e9c4b226b6ab733aa640334e
santhoshaveeravalli <71669927+santhoshaveeravalli@users.noreply.github.com>
2021-02-02 08:38:07 -0600

    Updated assignment 1 solution

---
+++ Assignment1/Assignment1Solution1/src/Assignment1.java
@@ -0,0 +1,76 @@
+class Vehicle {
+	void transport() {
+		System.out.println("Used for transportation");
+class Car{	
+	void audi() {
+		System.out.println("German automobile manufacturer");
+class Bike{
+	 void ktm() {
+		 System.out.println("Austrian Motocycle manufacturer");
+class Bicycle{
+	 void hero() {
+		 System.out.println("Indian Manufacturer");
+class Car1 {
+	void jeep() {
+		System.out.println("American automobile manufacturer");
+class CarPrice {
+	void audiPrice() {
+		System.out.println("Audi is 59800");
+class BikePrice {
+	void ktmPrice() {
+		System.out.println("Ktm is 9999");
+class BicyclePrice {
+	void heroPrice() {
+		System.out.println("Hero is 9800");
+class Car1Price{
+	void jeepPrice() {
+		System.out.println("Jeep is 40000");
+	}
+}
+Car1Price c1p = new Car1Price();
+c1p.jeepPrice();
+	}
+}
+BicyclePrice bcp = new BicyclePrice();
+bcp.heroPrice();
+	}
+      }
+BikePrice bp = new BikePrice();
+bp.ktmPrice();
+	}
+	}
+CarPrice cp = new CarPrice();
+cp.audiPrice();
+	}
+	}
+Car1 c1 = new Car1();
+c1.jeep();
+	 	 
+
+	 }
+}
+Bicycle bc = new Bicycle();
+bc.hero();
+
+	 }
+	 }
+Bike b = new Bike();
+b.ktm();
+	}
+	
+	}
+Car c = new Car();
+c.audi();
+	}
+}
+
+
+public class Assignment1 {
+
+	public static void main(String[] args) {
+		Vehicle v = new Vehicle();
+		v.transport() ;
+	}
+	}
+
+
