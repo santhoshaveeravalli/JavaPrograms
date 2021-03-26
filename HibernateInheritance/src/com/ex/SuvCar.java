398a53191fd068e15218e5b591d1f243dcad1ea8
santhoshaveeravalli <71669927+santhoshaveeravalli@users.noreply.github.com>
2021-03-26 09:48:59 -0500

    Updated Inheritance

---
+++ HibernateInheritance/src/com/ex/SuvCar.java
@@ -0,0 +1,13 @@
+package com.ex;
+
+public class SuvCar extends Vehicle{
+	private String suvcartype;
+
+	public String getSuvcartype() {
+		return suvcartype;
+	}
+
+	public void setSuvcartype(String suvcartype) {
+		this.suvcartype = suvcartype;
+	}
+}
---
+++ HibernateInheritance/src/com/ex/Vehicle.java
@@ -0,0 +1,36 @@
+package com.ex;
+
+public class Vehicle {
+	private int vehicle_id;
+	private String vehicle_name;
+	private String manufacturer;
+	private int price;
+	
+	
+	public int getVehicle_id() {
+		return vehicle_id;
+	}
+	public void setVehicle_id(int vehicle_id) {
+		this.vehicle_id = vehicle_id;
+	}
+	public String getVehicle_name() {
+		return vehicle_name;
+	}
+	public void setVehicle_name(String vehicle_name) {
+		this.vehicle_name = vehicle_name;
+	}
+	public String getManufacturer() {
+		return manufacturer;
+	}
+	public void setManufacturer(String manufacturer) {
+		this.manufacturer = manufacturer;
+	}
+	public int getPrice() {
+		return price;
+	}
+	public void setPrice(int price) {
+		this.price = price;
+	}
+	
+	
+}
